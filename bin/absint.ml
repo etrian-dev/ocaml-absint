a796f790bf20e3c7414a6441e39a7ee388b41808
Nicola Vetrini <nicola.vetrini@bugseng.com>
2023-07-05 23:57:48 +0200

    Added a few demos.

--- bin/absint.ml
+++ bin/absint.ml
@@ -4,5 +4,18 @@ let print_val idx v = Printf.printf "mem[%d] = %d\n" idx v
 
 let _ =
   let init_mem = Array.make 10 0 in
+  let newmem = sem_cmd Skip init_mem in
+  Printf.printf "Test 1:\n";
+  Array.iteri print_val newmem
+
+let _ =
+  let init_mem = Array.make 10 0 in
   let newmem = sem_cmd (Assign (1, Const 10)) init_mem in
+  Printf.printf "\nTest 2:\n";
+  Array.iteri print_val newmem
+
+let _ =
+  let init_mem = Array.make 10 0 in
+  let newmem = sem_cmd (Seq (Assign(1, Const 5), Assign(2, Bop(Mul, Var(1), Bop(Add, Const 2, Const 1)))) ) init_mem in
+  Printf.printf "\nTest 3:\n";
   Array.iteri print_val newmem
